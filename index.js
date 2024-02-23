c7e785ee22b7055aa024ae93b649a10c52ad81e2
Ericlkm <gtprinceeric.el1@gmail.com>
2024-02-23 03:38:40 -0500

    saving homework

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const createReadMe = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message:"Create title for project?"
+    },
+
+
+    {
+        type:"input",
+        name:"description",
+        message:"Write a detailed description of project and why is it important?"
+    },
+
+    {
+        type:"input",
+        name:"contribution",
+        message:"Who can contribute to this project?"
+    },
+
+    {
+        type:"input",
+        name:"install",
+        message:"What are the steps required to install your project?"
+    },
+
+    {
+        type:"input",
+        name:"intructions",
+        message:"Provide instructions and examples for use!"
+    },
+
+    {
+        type:"input",
+        name:"usage",
+        message:"Explain usage for this project on why, how and what the reson you're creating this project!",
+    },
+
+    {
+        type:"checkbox",
+        name:"license",
+        message:"which license is best for your project?",
+        choices:["MIT","GNU","Apache","MPL"]
+    },
+
+    {
+        type:"input",
+        name:"test",
+        message:"Give step by step on how user can test your project!"
+    },
+
+    {
+        type:"input",
+        name:"contact",
+        message:"what is your Github name?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+     fs.writeFile(fileName, data,err=>{
+        if(err) throw(err)
+        console.log(`\x1b[32m READE complete!\x1b[0m`)
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data)=>{
+        writeToFile("README.md", createReadMe(data))
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,56 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  #[![License](https://img.shields.io/${data.license}/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+  
+  - *[Description](#Description)*
+  - *[Installation](#Install)*
+  - *[Instruction](#Instruction)*
+  - *[contributing](#contribution)*
+  - *[Usage](#usage)*
+  - *[License](#License)*
+  - *[Testing](#test)*
+  - *[Contact](#contact)*
 
+
+
+  ## Description
+   **What was your motivation for this project?**
+   **what problem does it resolve?**
+   **what can be learned from this project?**
+
+  *${data.description}*
+ 
+  ## Installation
+  **steps to install this project!**
+
+  *${data.install}*
+ 
+  ## Instructions
+   **Instructions for use of this project!**
+
+  *${data.intructions}*
+  
+  ## Contributing
+  *${data.contribution}*
+  
+  ## Usage
+   **How will this project be used?**
+
+  *${data.usage}*
+  
+  ## License
+   **License for this project!**
+
+  *${data.license}*
+  
+  ## Testing
+  *${data.test}*
+  
+  ## Contact
+   *Contact informations!*
+
+   Github: (${data.contact}) https://github.com/${data.contact}
 `;
 }
 
